295cf375e75c438316a3ee7fde15155e04cc6264
LoOny <loonybiker@yandex.ru>
2015-09-30 08:33:39 +0300

    Исправление ошибок

--- Model.m
+++ Model.m
@@ -1,15 +1,18 @@
-function dy = Velocity(t, y)
-mass = 0.5;
-alpha = deg2rad(2);
-Vw = y(1);
+function dy = Model(t, y)
+%%CONST
+mass = 0.5;         % kg
+alpha = deg2rad(2); % rad
 Cx = 0.0162;
-S=1.2*0.38;
+S=1.2*0.38;         % m
 Cy = 0.266;
 P = 0.4;
+%
+
+Vw = y(1);
 
 X = Cx * 1.225 * (Vw^2 / 2) *S;
 Y = Cy * 1.225 * (Vw^2 / 2) *S;
-G = mass*9.81;
+G = mass*9.81;                  % H
 
 dy = zeros(3, 1);
     dy(1) =  -X/mass - G*sin(y(2))/mass + P*cos(alpha)/mass;
--- main.m
+++ main.m
@@ -1,4 +1,4 @@
-п»їclc;
+clc;
 clear all;
 
 y = zeros(3,1);
@@ -6,7 +6,7 @@ y(1) = 5;
 y(2) = deg2rad(5);
 y(3) = 5;
 
-[T,Y] = ode45(@Velocity, [0:0.1:10], y);
+[T,Y] = ode45(@Model, [0:0.1:10], y);
 
 figure % new figure
 ax1 = subplot(3,1,1); % top subplot
@@ -15,13 +15,13 @@ ax3 = subplot(3,1,3); % bottom subplot
 
 plot(ax1, T, Y(:,1))
 grid on
-title(ax1,'РР·РјРµРЅРµРЅРёРµ СЃРєРѕСЂРѕСЃС‚Рё')
-ylabel(ax1,'РЎРєРѕСЂРѕСЃС‚СЊ')
+title(ax1,'Изменение скорости')
+ylabel(ax1,'м/с')
 
 plot(ax2, T, rad2deg(Y(:,2)))
-title(ax2,'РР·РјРµРЅРµРЅРёРµ СѓРіР»Р° РЅР°РєР»РѕРЅР° С‚СЂР°РµРєС‚РѕСЂРёРё Рє РіРѕСЂРёР·РѕРЅС‚Сѓ')
-ylabel(ax2,'РЈРіРѕР» (РіСЂР°РґСѓСЃС‹)')
+title(ax2,'Изменение угла наклона плоскости к горизонту')
+ylabel(ax2,'Угол (град)')
 
 plot(ax3, T, Y(:,3))
-title(ax3,'РР·РјРµРЅРµРЅРёРµ РІС‹СЃРѕС‚С‹')
-ylabel(ax3,'РњРµС‚СЂС‹')
+title(ax3,'Изменение всоты‹')
+ylabel(ax3,'Высота (м)')
