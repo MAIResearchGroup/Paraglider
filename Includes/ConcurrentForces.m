1eacf5f334d65b5c925d1abf050bf101ff8e3005
ElMigele <michail.1994@mail.ru>
2016-03-17 20:03:38 +0300

    Рабочая версия ConcurrentForces
    Сегодня довели пограмму до ума, прогнали по всем возможным ситуациям.
    Выдает правильный результат

--- Includes/ConcurrentForces.m
+++ Includes/ConcurrentForces.m
@@ -3,31 +3,48 @@ function [ resF, resP ] = ConcurrentForces(F1, pos1, F2, pos2)
     if (sign(F1) == sign(F2))
         if F1 > F2
             k = F2/F1;
+            pos2_ = pos2 - pos1;
+            resP_ = pos2_ * k;
+            resP  = resP_ + pos1;
         else
-            k = F1/F2;
+            if (F1 == F2)
+                resP = (pos1 + pos2)/2;
+            else   
+                k = F1/F2;
+                pos1_ = pos1 - pos2;
+                resP_ = pos1_ * k;
+                resP  = resP_ + pos2;
+            end
         end
-        pos2_ = pos2 - pos1;
-        resP_ = pos2_ * k;
-        resP  = resP_ + pos1;
     else
         if abs(F1) > abs(F2)
-            if abs(F2) > (F1 - F2)
-                k = abs(F2)/abs(F1 - abs(F2));
+            if abs(F2) > (abs(F1) - abs(F2))
+                k =(abs(F1) - abs(F2))/abs(F2);
+                pos1_ = pos1 - pos2;
+                resP_ = pos1_ * k;
+                resP  = resP_ + pos1;
             else
-                k = abs(F1 - abs(F2))/abs(F2);
+                k = abs(F2)/(abs(F1) - abs(F2));
+                pos1_ = pos1 - pos2;
+                resP_ = pos1_ * k;
+                resP  = resP_ + pos1;
             end
-        pos1_ = pos1 - pos2;
-        resP_ = pos1_ * k;
-        resP  = resP_ + pos2;
         else
-            if abs(F1) > abs(F2 - abs(F1))
-                k = abs(F1)/abs(F2 - abs(F1));
+            if (abs(F1) == abs(F2))
+                resP = pos1;
             else
-                k = abs(F2 - abs(F1))/abs(F1);
+                if abs(F1) > (abs(F2) - abs(F1))
+                    k = (abs(F2) - abs(F1))/abs(F1);
+                    pos2_ = pos2 - pos1;
+                    resP_ = pos2_ * k;
+                    resP  = resP_ + pos2;
+                else
+                    k = -abs(F1)/(abs(F2) - abs(F1));
+                    pos1_ = pos1 - pos2;
+                    resP_ = pos1_ * k;
+                    resP  = resP_ + pos2;
+                end
             end
-        pos2_ = pos2 - pos1;
-        resP_ = pos2_ * k;
-        resP  = resP_ + pos1;
         end
     end
 end
--- Includes/Intitled2.m
+++ Includes/Intitled2.m
@@ -1,5 +1,5 @@
-Force1 = 10;
-Force2 = 30;
-pos1   = [1, 1];
-pos2   = [-2, -2];
+Force1 = 2;
+Force2 = 10;
+pos1   = [0, 2.5];
+pos2   = [0, 0];
 [A B]= ConcurrentForces(Force1, pos1, Force2, pos2)
