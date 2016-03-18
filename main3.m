ecce0258b7a0c08a9fba3b8e3fa7ad98bc4cede9
LoOny <loonybiker@yandex.ru>
2016-03-18 10:18:03 +0300

    Разрешил merge conflict

---
+++ main3.m
@@ -0,0 +1,40 @@
+clc; close all;
+%% Рабочие каталоги
+PROFILES = strcat(pwd, '\Profiles');
+INCLUDES = strcat(pwd, '\Includes');
+addpath(PROFILES, INCLUDES);
+%%  Интегрирование
+%   Исходные данные
+    r      = [0;   0];      % Положение, м
+    v      = [2;   0];      % Скорость, м/c
+    attack = deg2rad(2);    % Угол атаки, рад
+%   Параметры
+    ITime = 600;              % Время интегрирования, с
+    IStep = 0.1;            % Шаг интегрирования
+    [T,Y] = ode45(@Model3, 0:IStep:ITime, [r; v; attack]);
+%% Вывод данных
+    figure(1)
+        subplot(2,2,1);
+            plot(T, Y(:, 1));
+            grid on
+            title('Изменение положения аппарата по оси Х от времени');
+            xlabel('Время, с');
+            ylabel('X, м');
+        subplot(2,2,2);
+            plot(T, Y(:, 2));
+            grid on
+            title('Изменение положения аппарата по оси Y от времени');
+            xlabel('Время, с');
+            ylabel('Y, м');
+        subplot(2,2,3);
+            plot(T, Y(:, 3));
+            grid on
+            title('Изменение скорости аппарата по оси Х от времени');
+            xlabel('Время, с');
+            ylabel('V_x, м/с');
+        subplot(2,2,4);
+            plot(T, Y(:, 4));
+            grid on
+            title('Изменение скорости аппарата по оси Y от времени');
+            xlabel('Время, с');
+            ylabel('V_y, м/с');
