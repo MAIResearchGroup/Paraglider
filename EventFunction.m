488e865ad0fa90211101f8ed66a075f47ccb3081
LoOny <loonybiker@yandex.ru>
2016-03-27 17:04:52 +0300

    Удалил устаревшие файлы

--- EventFunction.m
+++
@@ -1,9 +0,0 @@
-function [value,isterminal,direction] = EventFunction(~,X)
-    value = X(4)-0;
-    
-    %Stop if the hit the ground
-    isterminal = true;
-
-    %Should only be coming to the ground from above
-    direction = -1;
-end
