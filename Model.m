537e536ac5ccebe688410438697afe5a360e0111
ElMigele <michail.1994@mail.ru>
2016-03-30 21:35:27 +0300

    Исправление ошибки в модели взлёта

--- Model.m
+++ Model.m
@@ -17,15 +17,14 @@ function r = Model( t, state, P, Env )
     Y = P.Wing.Cy * Env.AirDen*(vel(1)^2)/2 * P.Wing.S;
     X = P.Wing.Cx * Env.AirDen*(vel(1)^2)/2 * P.Wing.S;
 
-    pos(1) = vel(1);
-    if pos(2) <= 0 && vel(2) < 0
-        pos(2) = 0;
-    else
-        pos(2) = vel(2);
-    end
+
     dV = (P.Thrust * [cos(theta); sin(theta)] + [-X; Y-P.Weight])/P.SMass;
+    if (pos(2) <= 0) && (dV(2) < 0)
+        dV(2) = 0;
+    end
+    pos = vel;
     dtheta  = (P.Thrust *(sin(attack)) + Y - P.Weight *cos(theta))/(P.SMass * Vel);
     dattack = 0;
     
-    r = [dV(1); dV(2); pos(1); pos(2); dtheta; dattack];
+    r = [dV; pos; dtheta; dattack];
 end
