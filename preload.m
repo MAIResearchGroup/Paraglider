dcf5e377491f38d10de739b61d320c2794b4afc3
LoOny <loonybiker@yandex.ru>
2016-04-01 10:57:25 +0300

    Добавлена загрузка данных

---
+++ preload.m
@@ -0,0 +1,17 @@
+%% Настройка рабочих каталогов
+PROFILES = strcat(pwd, '\Profiles');
+INCLUDES = strcat(pwd, '\Includes');
+addpath(PROFILES, INCLUDES);
+
+PATH = strcat(pwd, '\.data');
+addpath( PATH )
+    % Загружаем все данные в Workspace, например, стандартную атмосферу.
+rmpath( PATH )
+
+if useCBSAF
+    cbsaf;
+end
+
+if verify
+    verification;
+end
